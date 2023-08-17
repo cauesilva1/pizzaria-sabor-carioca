ff5fa4bb272cf393c11771c057ebcbe67accb59e
Cauê catone silva <cauecatonesilva@gmail.com>
2023-08-17 10:33:04 -0400

    add: sidebar in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { CardList } from "@/components/CardList";
-
-import Navbar from "@/components/Navbar";
+import { CardList } from "@/components/CardList"; 
 import UltimosPedidos from "@/components/UltimosPedidos";
 
 
@@ -8,10 +6,6 @@ import UltimosPedidos from "@/components/UltimosPedidos";
 export default function Home() {
   return (
     <div className="flex w-screen justify-between relative">
-
-
-      <Navbar />
-      
       <div className="flex flex-col w-full justify-center h-full items-center ml-28">
 
       <CardList />
